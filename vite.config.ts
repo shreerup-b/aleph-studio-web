c11b86ab57428f021c18f88384a8d6ef01cf2bac
Shreerup <banerjeeshreerup@gmail.com>
2026-03-31 13:12:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,3 +8,11 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/aleph-studio-web/',
+})
